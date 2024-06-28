a8dd104155f253ac0214d81acf87b54f561f8eda
gabrielseiko <gabriel-s98@hotmail.com>
2024-06-28 01:31:20 -0500

    CRUD AUTORES

--- src/app/components/autor/autor-actualizar/autor-actualizar.component.ts
+++ src/app/components/autor/autor-actualizar/autor-actualizar.component.ts
@@ -1,12 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MenuComponent } from '../../../menu/menu.component';
+import { CommonModule } from '@angular/common';
+import { AppMaterialModule } from '../../../app.material.module';
+import { Autor } from '../../../models/autor.model';
+import { AutorService } from '../../../services/autor.service';
+import { TokenService } from '../../../security/token.service';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-autor-actualizar',
   standalone: true,
-  imports: [],
+  imports: [AppMaterialModule, FormsModule, CommonModule, MenuComponent, ReactiveFormsModule],
   templateUrl: './autor-actualizar.component.html',
   styleUrl: './autor-actualizar.component.css'
 })
 export class AutorActualizarComponent {
+  autor: Autor = {
+    idAutor: -1,
+    nombreCompleto: ""
+  }
 
+  formRegistrar = this.formBuilder.group({
+    validaNombres: ['', [Validators.required, Validators.pattern('[a-zA-Z ]{3,30}')]]
+  });  
+
+  constructor(
+    private autorService: AutorService,
+    private tokenService: TokenService,
+    private formBuilder: FormBuilder,
+    @Inject(MAT_DIALOG_DATA) public data: Autor 
+  ) {this.autor = data;} 
+
+  actualizar() {
+    this.autorService.actualizarAutor(this.autor).subscribe(
+      x=>{
+            Swal.fire({ icon: 'info', title: 'Resultado del Registro', text: x.mensaje, });
+            this.autor ={
+              idAutor: -1,
+              nombreCompleto: "",
+                }
+        }
+    );
+  }
+  
+  resetForm() {
+    this.autor = {
+      idAutor: -1,
+      nombreCompleto: "",
+    };
+    this.formRegistrar.reset();
+  } 
+  salir() {
+          
+  }  
 }
--- src/app/components/autor/autor-listar/autor-listar.component.ts
+++ src/app/components/autor/autor-listar/autor-listar.component.ts
@@ -1,12 +1,109 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MenuComponent } from '../../../menu/menu.component';
+import { CommonModule } from '@angular/common';
+import { AppMaterialModule } from '../../../app.material.module';
+import { MatTableDataSource } from '@angular/material/table';
+import { Autor } from '../../../models/autor.model';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatDialog } from '@angular/material/dialog';
+import { TokenService } from '../../../security/token.service';
+import { AutorService } from '../../../services/autor.service';
+import Swal from 'sweetalert2';
+import { AutorAgregarComponent } from '../autor-agregar/autor-agregar.component';
+import { AutorActualizarComponent } from '../autor-actualizar/autor-actualizar.component';
 
 @Component({
   selector: 'app-autor-listar',
   standalone: true,
-  imports: [],
+  imports: [AppMaterialModule, FormsModule, CommonModule, MenuComponent, ReactiveFormsModule],
   templateUrl: './autor-listar.component.html',
-  styleUrl: './autor-listar.component.css'
+  styleUrls: ['./autor-listar.component.css']
 })
-export class AutorListarComponent {
+export class AutorListarComponent implements OnInit, AfterViewInit {
 
+  dataSource = new MatTableDataSource<Autor>();
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  displayedColumns = [
+    "idAutor",
+    "nombreCompleto",
+    "acciones"
+  ]; 
+
+  listaAutores: Autor[] = [];
+
+  constructor(
+    private dialogService: MatDialog,
+    private autorService: AutorService,
+    private tokenService: TokenService
+  ) {}
+
+  ngOnInit() {
+    this.refreshTable();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+  openDialogRegistrar() {
+    const dialogRef = this.dialogService.open(AutorAgregarComponent, {});
+  
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('Dialog closed with result:', result);
+      if (result != null && result === 1) {
+        this.refreshTable();
+      }
+    });
+  }
+
+  openDialogActualizar(obj: Autor) {
+    console.log(">>> openDialogActualizar [ini]");
+    console.log("obj: ", obj);
+    const dialogRef = this.dialogService.open(AutorActualizarComponent, {data: obj} );
+    dialogRef.afterClosed().subscribe(result => {
+        console.log('Dialog closed with result:', result);
+        if (result != null && (result === 1 || result === 2)) {
+              this.refreshTable();
+        }
+    });
+    console.log(">>> openDialogActualizar [fin]");
+  }  
+
+  refreshTable() {
+    this.autorService.listarAutores().subscribe(
+      data => {
+        this.listaAutores = data;
+        this.dataSource.data = this.listaAutores;
+      },
+      error => {
+        console.error('Error al listar Autores:', error);
+      }
+    );
+  }
+
+  delete(obj: Autor) {
+    Swal.fire({
+      title: '¿Desea eliminar?',
+      text: "Los cambios no se van a revertir",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, elimina',
+      cancelButtonText: 'No, cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.autorService.eliminarAutor(obj.idAutor || 0).subscribe(
+          x => {
+            Swal.fire('Mensaje', x.mensaje, 'info');
+            this.refreshTable();
+          },
+          error => {
+            Swal.fire('Error', 'No se pudo eliminar el Autor', 'error');
+          }
+        );
+      }
+    });
+  }
 }
